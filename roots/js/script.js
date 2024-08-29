87d4476df51c2c110b2bdd3b186d56e04703c028
Erika Santana da Silva <24026205@fecap.br>
2024-08-28 22:30:39 -0300

    finalizando html e css

--- roots/js/script.js
+++ roots/js/script.js
@@ -1,7 +1,29 @@
+let currentSlide = 0;
 
-    const menuIcon = document.querySelector('.menu-icon');
-    const navbar = document.querySelector('.navbar');
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    const totalSlides = slides.length;
 
-    menuIcon.addEventListener('click', () => {
-        navbar.classList.toggle('active');
-    });
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const offset = -currentSlide * 100 / 3; // 3 itens por slide
+    const carouselInner = document.querySelector('.carousel-inner');
+    carouselInner.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Inicializa o carrossel
+showSlide(currentSlide);
